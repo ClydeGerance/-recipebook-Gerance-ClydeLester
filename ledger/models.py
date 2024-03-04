084c2d57492e082058d1151a685bc0f538cff2bb
ClydeGerance <clyde.gerance@student.ateneo.edu>
2024-03-04 23:24:06 +0800

    added content for models.py

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ledger:recipe_detail', args={'pk': self.pk})
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
+    quantity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name}"
