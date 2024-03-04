83ff12a8ed6c0e2de00831e965c6c148c0f3b4fa
ClydeGerance <clyde.gerance@student.ateneo.edu>
2024-03-04 23:24:43 +0800

    added content for views.py

--- ledger/views.py
+++ ledger/views.py
@@ -1,31 +1,20 @@
-from django.shortcuts import render
-import os
-import json
-from pathlib import Path
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-data_dir = os.path.join(BASE_DIR, 'data')
-templates_dir = os.path.join(BASE_DIR, 'templates')
+from .models import Recipe
 
-def recipe_list(request):
-    recipe_list_file = os.path.join(data_dir, 'Recipe List Context.txt')
-    with open(recipe_list_file, 'r') as file:
-        ctx = {'recipe_list': file.read().split('\n')}
-    html_file_path = os.path.join(templates_dir, 'recipe_list.html')
-    return render(request, html_file_path, ctx)
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipe_list.html'
+    context_object_name = 'recipes'
 
-def recipe_detail_1(request):
-    recipe_detail_file = os.path.join(data_dir, 'Recipe 1.txt')
-    with open(recipe_detail_file, 'r') as file:
-        recipe_detail = json.load(file)
-    ctx = {'recipe_detail': recipe_detail}
-    html_file_path = os.path.join(templates_dir, 'recipe_detail_1.html')
-    return render(request, html_file_path, ctx)
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe_detail.html'
+    context_object_name = 'recipe'
 
-def recipe_detail_2(request):
-    recipe_detail_file = os.path.join(data_dir, 'Recipe 2.txt')
-    with open(recipe_detail_file, 'r') as file:
-        recipe_detail = json.load(file)
-    ctx = {'recipe_detail': recipe_detail}
-    html_file_path = os.path.join(templates_dir, 'recipe_detail_2.html')
-    return render(request, html_file_path, ctx)
+    def get_context_data(self, **args):
+        context = super().get_context_data(**args)
+        ingredients = self.object.ingredients.all()
+        context['recipe_ingredients'] = ingredients
+        return context
