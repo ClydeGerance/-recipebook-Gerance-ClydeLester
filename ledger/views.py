d7704d0f6c0172282c41cb984062d262b3446179
ClydeGerance <clyde.gerance@student.ateneo.edu>
2024-03-15 20:16:18 +0800

    edited for lab 3

--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Recipe
 
 from .models import Recipe
 
@@ -12,9 +18,15 @@ class RecipeDetailView(DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
     context_object_name = 'recipe'
+    login_url = reverse_lazy('login')
 
     def get_context_data(self, **args):
         context = super().get_context_data(**args)
         ingredients = self.object.ingredients.all()
         context['recipe_ingredients'] = ingredients
         return context
+
+class CustomLoginView(DjangoLoginView):
+    def get_success_url(self) -> str:
+        return reverse_lazy('recipe_list')
+            
